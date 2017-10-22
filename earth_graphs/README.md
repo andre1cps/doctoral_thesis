a416ce46f8e5a7ae7fcbc0c71263910eceaaca39
Alex Sandro <alex.fate2000@gmail.com>
2017-10-22 21:26:42 -0200

    Initial commit

---
+++ earth_graphs/README.md
@@ -0,0 +1,9 @@
+# Earth graphs
+
+Here I am testing some functions in python 3 intended to study complex networks representation for Earth climate.
+
+**Author: Alex Sandro Alves de Araujo**
+
+**Email: alex.fate2000@gmail.com** 
+
+
---
+++ earth_graphs/__init__.py
@@ -0,0 +1,18 @@
+"""
+Earth graphs
+============
+
+Here are functions dealing with graph representation for Earth 
+surface.
+
+Under construction.
+
+Author: Alex Sandro Alves de Araujo
+Email: alex.fate2000@gmail.com
+
+"""
+
+from . import grids
+from . import benchmark_network
+
+__all__ = ["grids", "benchmark_network"]
